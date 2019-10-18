357897b1eb348a964e5197d2a0e4c3c622ce6b1a
Fontão <up201806317@fe.up.pt>
2019-10-18 18:24:39 +0100

    Added constructors for all classes and the virtual method calcCost()

--- Headers/Activity.h
+++ Headers/Activity.h
@@ -11,21 +11,37 @@ class Activity {
         Activity(Time startTime,Time endTime);
         Time getStartTime();
         Time getEndTime();
+        virtual int calcCost() const;
 };
 
 class Ride : public Activity {
+public:
+    Ride(Time startTime,Time endTime) : Activity(startTime,endTime){};
+    int CalcCost() const;
 };
 
 class Lesson : public Activity {
+public:
+    int CalcCost() const;
+    Lesson(Time startTime,Time endTime) : Activity(startTime,endTime){};
 };
 
 class StandUpPaddle : public Lesson {
+public:
+    int CalcCost() const;
+    StandUpPaddle(Time startTime,Time endTime) : Lesson(startTime,endTime){};
 };
 
 class Surf : public Lesson {
+public:
+    int CalcCost() const;
+    Surf(Time startTime,Time endTime) : Lesson(startTime,endTime){};
 };
 
 class Windsurf : public Lesson {
+public:
+    int CalcCost() const;
+    Windsurf(Time startTime,Time endTime) : Lesson(startTime,endTime){};
 };
 
 #endif //SUP_SCHOOL_ACTIVITY_H
