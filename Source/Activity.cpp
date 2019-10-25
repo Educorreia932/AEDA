d24ca279d6e4d5eca649f18dee3c7aa44c6279cd
Fontão <up201806317@fe.up.pt>
2019-10-25 13:48:22 +0100

    Implemented activityNonExistant exception

--- Source/Activity.cpp
+++ Source/Activity.cpp
@@ -3,9 +3,12 @@
 
 using namespace std;
 
+unsigned int Activity::id = 0;
+
 Activity::Activity(Time startTime, Time endTime) {
     this->startTime = startTime;
     this->endTime = endTime;
+    this->id = Activity::id++;
 }
 
 Time Activity::getStartTime() {
@@ -14,4 +17,13 @@ Time Activity::getStartTime() {
 
 Time Activity::getEndTime() {
     return endTime;
+}
+
+unsigned int Activity::getId() const {
+    return this->id;
+}
+
+std::ostream &operator<<(std::ostream &out, const activityNonExistant &activity) {
+    out << "Activity with ID \"" << activity.id << "\" does not exist." << endl;
+    return out;
 }
